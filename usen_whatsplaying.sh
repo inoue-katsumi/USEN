052de67f4cecb7ed46fd075523143949f62f265a
Katsumi INOUE <kinoue@LAPTOP-62SD4QOU.localdomain>
2018-04-07 00:17:24 +0900

    Created unique list using q. Modified the script to look up new file.

--- usen_whatsplaying.sh
+++ usen_whatsplaying.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 [ -z $3 ] && echo "Usage: $0 (YYYYMMDD) (HH:MI:SS) (any string to filter usen channel list)" && exit 1
-curl --silent  https://raw.githubusercontent.com/inoue-katsumi/USEN/master/usen_channels_genre.csv |
+curl --silent  https://raw.githubusercontent.com/inoue-katsumi/USEN/master/usen_channels_genre_uniq.csv |
 grep $3 |
 awk -F',' '{print $2,$3,$1}' | tr -d '"' |
 while read band npch name
